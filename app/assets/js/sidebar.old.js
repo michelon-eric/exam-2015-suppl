a71de04bb7d22fe358a97ccbbdc037205f6afc73
michelon-eric <michelon.eric.2005@gmail.com>
2024-02-24 19:38:21 +0100

    hack language cleanup

--- app/assets/js/sidebar.old.js
+++
@@ -1,6 +0,0 @@
-$('.uncollapse').each(function () {
-	$(this).on('click', function () {
-		$(this).find('#collapse-icon').toggleClass('rotate')
-		// $(this).parent().find('div').toggleClass('show')
-	})
-})
--- app/views/layout/navbar.php
+++ app/views/layout/navbar.php
@@ -1,3 +1,5 @@
+<?php /* */ ?>
+
 <header class="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white text-sm py-3 sm:py-0 dark:bg-gray-950">
     <nav class="relative max-w-[85-rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8" aria-label="Global">
         <div class="flex items-center justify-between">
